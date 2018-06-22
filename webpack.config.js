6e4d76d61e65f2a04d00ca77c1cc00fb45e7e59e
chris <12405301@qq.com>
2018-06-23 01:28:51 +0800

    多入口

--- webpack.config.js
+++ webpack.config.js
@@ -8,10 +8,11 @@ module.exports = {
 
   devtool: 'eval-source-map',
 
+  /*
   //入口文件
   entry: "./src/index.js",
 
-  //webpakc如何输出的目标路径
+  //出口文件的配置项
   output: {
     // 所有输出文件的目标路径
     // 必须是绝对路径（使用 Node.js 的 path 模块）
@@ -20,6 +21,26 @@ module.exports = {
     //输出文件的名字
     filename : 'bundle.js',
   },
+  */
+
+  //入口文件
+  entry:{
+    index: './src/index.js',
+    greeter: './src/greeter.js'
+  },
+  //出口文件的配置项
+  output:{
+    path:path.resolve(__dirname,'dist'),
+    //filename前面我们可以使用一个变量[name],这个就表示获取entry里面的key作为文件名加在前面
+    //打出来是index.js和index2.js
+    filename:'[name].js'
+  },
+
+  //模块：例如解读CSS,图片如何转换，压缩
+  module:{},
+  //插件，用于生产模版和各项功能
+  plugins:[],
+  //配置webpack开发服务功能
   devServer: {
     // 设置服务器访问的基本目录
     contentBase:path.resolve(__dirname,'dist'), //最好设置成绝对路径
